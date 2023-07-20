fa39825cb9e4c87be3bf809fe4c52b059ca51ef4
Divyanshu Kumar <4674kumardivyanshu@gmail.com>
2023-07-21 01:42:00 +0530

    CHeckbox Bobby

--- details.js
+++ details.js
@@ -1,25 +1,9 @@
-function verifyAge() {
-  var age = prompt("Are you 18 years or older? (Yes/No)").toLowerCase();
-
-  if (age === "yes") {
-    return "slot_machine.html";
-  } else {
-    return "not_allowed.html";
-  }
-}
-
-// console.log(typeof verifyAge()); // "function"
-
 const detailsForm = document.querySelector("#details-form");
 
 if (detailsForm) {
   detailsForm.addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent form submission
 
-    // Call the verifyAge() function
-    var redirectUrl = verifyAge();
-
-    // Redirect to the appropriate page
-    window.location.href = redirectUrl;
+    window.location.href = "slot_machine.html";
   });
 }
